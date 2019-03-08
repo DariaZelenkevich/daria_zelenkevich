d5ee5e41902776e5c155cfa64f1cc3307105c36a
DariaZelenkevich <dariazelenkevich@gmail.com>
2019-03-09 00:24:17 +0300

    modal-form open/close script added

--- final_project/index.js
+++ final_project/index.js
@@ -1,47 +1,58 @@
+//modal form
+
+var modal = document.getElementById("modal");
+var modalOpenButton = document.getElementById("modal_open_button");
+var modalCloseButton = document.getElementById("modal_close_button");
+
+modalOpenButton.addEventListener('click', function () {
+  modal.style.display = 'flex';
+});
+
+modalCloseButton.addEventListener('click', function () {
+  modal.style.display = 'none';
+});
+
+
+//service items
 var servicesItems = document.getElementsByClassName("services__item");
 var servicesItem;
+
 
 for ( var j = 0; j < servicesItems.length; j++) {
   servicesItem = servicesItems[j];
 
   var infoCards = servicesItem.getElementsByClassName("item__info");
-  var infoCard = infoCards[0];
+  var infoCard;
 
   var openCardLinks = servicesItem.getElementsByClassName("item__openlink");
   var openCardLink;
 
-  for ( var i = 0; i < openCardLinks.length; i++) {
-    openCardLink = openCardLinks[i];
-    openCardLink.addEventListener('click',  function () {
-      infoCard.classList.remove("invisible");
-    });
-  }
-
   var closeCardLinks = servicesItem.getElementsByClassName("item__close-button");
   var closeCardLink;
 
-  for ( var n = 0; n < closeCardLinks.length; n++) {
-    closeCardLink = closeCardLinks[n];
-    closeCardLink.addEventListener('click',  function () {
-      infoCard.classList.add("invisible");
-    });
+
+
+
+  for (var m = 0; m < infoCards.length; m++) {
+    infoCard = infoCards[m];
+
+    for ( var i = 0; i < openCardLinks.length; i++) {
+        openCardLink = openCardLinks[i];
+        openCardLink.addEventListener('click', showInfoCard);
+    }
+
+    for ( var n = 0; n < closeCardLinks.length; n++) {
+        closeCardLink = closeCardLinks[n];
+        closeCardLink.addEventListener('click', hideInfoCard);
+        console.log('added ' + n);
+    }
   }
-
-
-
-  // function toggleInfoCard() {
-  //   infoCard.style.display = (infoCard.style.display == 'none') ? 'block' : 'none';
-  // }
-
-//   if (infoCard.classList.contains("invisible")) {
-//     infoCardclassList.remove("invisible");
-//   }
-//   else {
-//     infoCard.classList.add("invisible");
-//   }
-// });
 }
 
+function hideInfoCard () {
+  infoCard.classList.add("invisible");
+}
 
-
-
+function showInfoCard () {
+  infoCard.classList.remove("invisible");
+}
